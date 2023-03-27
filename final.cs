d1e703ce0ec4addd7072d43c46825314528208c5
Oksana <oksana-k83@mail.ru>
2023-03-27 15:14:19 +0300

    Задача 2 и 3  финального проекта

--- final.cs
+++ final.cs
@@ -4,27 +4,57 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите целое число больше 1: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-NaturalCount(number, count);
+// Console.Write("Введите целое число больше 1: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// NaturalCount(number, count);
 
-void NaturalCount(int n, int count)
-{
-    if (count > n)
-    {
-        return;
-    }
-    else
-    {
-        NaturalCount(n, count + 1);
-        Console.Write(count + " ");
-    }
-}
+// void NaturalCount(int n, int count)
+// {
+//     if (count > n)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         NaturalCount(n, count + 1);
+//         Console.Write(count + " ");
+//     }
+// }
 
 // Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int temp = m;
+
+// if (m > n)
+// {
+//     m = n;
+//     n = temp;
+// }
+
+// PrintSumm(m, n, temp = 0);
+
+// void PrintSumm(int m, int n, int summ)
+// {
+//     summ = summ + n;
+//     if (n <= m)
+//     {
+//         Console.Write($"Сумма элементов = {summ} ");
+//         return;
+//     }
+//     PrintSumm(m, n - 1, summ);
+// }
+
+// int InputNumbers(string input)
+// {
+//     Console.Write(input);
+//     int output = Convert.ToInt32(Console.ReadLine());
+//     return output;
+// }
 
 
 
@@ -32,3 +62,24 @@ void NaturalCount(int n, int count)
 // Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
